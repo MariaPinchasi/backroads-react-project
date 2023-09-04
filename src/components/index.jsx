dc985ede621ffb36fbdadc68e931c29fbbe8a5fe
MariaPinchasi <135972701+MariaPinchasi@users.noreply.github.com>
2023-09-04 14:35:40 +0300

    errorFix2

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,7 +3,7 @@ import Hero from "./Hero";
 import About from "./About";
 import Services from "./Services";
 import Tours from "./Tours";
-import Footer from "./footer";
 import Title from "./Title";
+import Footer from "./Footer";
 
 export { Navbar, Hero, About, Services, Tours, Footer, Title };
